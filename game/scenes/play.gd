383f202c3532e8050e466ceefc6679947d0c5cd7
unknown <neclusive@gmail.com>
2025-09-04 17:47:18 -0500

    Revert "added transitions and loading screens"
    This reverts commit 7cd6f1bca173af440a0be12732985d92bb1616af.

--- game/scenes/play.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	Fade.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
-	Fade.transition_in()
-	await Fade.on_animation_finished
-	var new_scene = load("res://scenes/loading.tscn")
-	get_tree().change_scene_to_packed(new_scene)
